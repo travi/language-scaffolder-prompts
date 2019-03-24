9f6f3ef4af2147eb13930325e9f40a77660483de
Matt Travi <programmer@travi.org>
2019-03-23 23:25:06 -0500

    feat(questions): included unit-testing confirmation and ci-service choice
    and exposed constants for the question names to simplify use of answers in the consumers

---
+++ src/index.js
@@ -0,0 +1,2 @@
+export {default as questions} from './questions';
+export {questionNames} from './question-names';
---
+++ test/unit/questions-test.js
@@ -0,0 +1,48 @@
+import {Separator} from 'inquirer';
+import {assert} from 'chai';
+import any from '@travi/any';
+import sinon from 'sinon';
+import * as choicesVisibilityFilter from '../../src/filter-by-visibility';
+import {questions, questionNames} from '../../src';
+
+suite('questions', () => {
+  let sandbox;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(choicesVisibilityFilter, 'default');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that the confirmation about unit-testing is included', () => {
+    assert.deepEqual(
+      questions({}),
+      [{
+        name: questionNames.UNIT_TESTS,
+        message: 'Will this project be unit tested?',
+        type: 'confirm',
+        default: true
+      }]
+    );
+  });
+
+  test('that the ci-service choice is included when a project will be versioned', () => {
+    const ciServices = any.simpleObject();
+    const visibility = any.word();
+    const filteredCiServiceNames = any.listOf(any.word);
+    const filteredCiServices = any.objectWithKeys(filteredCiServiceNames);
+    choicesVisibilityFilter.default.withArgs(ciServices, visibility).returns(filteredCiServices);
+
+    assert.deepInclude(
+      questions({vcs: any.simpleObject(), ciServices, visibility}),
+      {
+        name: questionNames.CI_SERVICE,
+        type: 'list',
+        message: 'Which continuous integration service will be used?',
+        choices: [...filteredCiServiceNames, new Separator(), 'Other']
+      }
+    );
+  });
+});
